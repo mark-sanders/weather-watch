1b721222dfbe009b7788f79bb1cd1125110e45f8
Mark Sanders <masander@capgemini.com>
2016-05-19 19:44:36 +0100

    Adding example Hong Kong result and additional error responses

--- src/main/java/com/github/marksanders/weatherwatch/api/Weather.java
+++ src/main/java/com/github/marksanders/weatherwatch/api/Weather.java
@@ -5,13 +5,31 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Weather {
 
-    private String name;
+    private String main;
+    private String description;
+    private String icon;
 
-    public String getName() {
-        return name;
+    public String getMain() {
+        return main;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+    
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 }
