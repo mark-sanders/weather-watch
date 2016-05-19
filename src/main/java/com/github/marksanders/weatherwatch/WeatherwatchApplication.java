f22f2d471070453604f6cee67453fb59873c3539
Mark Sanders <masander@capgemini.com>
2016-05-19 16:49:00 +0100

    initial Spring Boot starter project using Web dependencies and based on package name com.github.marksanders.weatherwatch

---
+++ src/main/java/com/github/marksanders/weatherwatch/WeatherwatchApplication.java
@@ -0,0 +1,12 @@
+package com.github.marksanders.weatherwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherwatchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/marksanders/weatherwatch/WeatherwatchApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.marksanders.weatherwatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WeatherwatchApplication.class)
+@WebAppConfiguration
+public class WeatherwatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
